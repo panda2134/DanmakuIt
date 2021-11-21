c7226901a782f6b479f8eb385ffedb29f5f0e6dd
YouJiacheng <1503679330@qq.com>
2021-11-21 19:04:01 +0800

    feat: more useful ws_client example

--- test/ws_client.py
+++ test/ws_client.py
@@ -1,13 +1,53 @@
 import json
+
+import signal
+from threading import Thread
+from time import sleep
+
 import websocket
 
 
+def on_message(ws: websocket.WebSocketApp, message: str):
+    print(message)
+    if message == '':
+        ws.close()
+        return
+    msg = json.loads(message)
+    print(msg['payload'])
+    print(msg['properties'])
+    ws.send(json.dumps({'messageId': msg['messageId']}))
     
 
+def on_error(ws, error):
+    print(error)
 
-ws = websocket.create_connection('ws://localhost:8080/ws/v2/consumer/persistent/public/default/3/testsub')
+
+def on_close(ws, close_status_code, close_msg):
+    print("### close ###")
+
+
+def on_open(ws):
+    print("### open ###")
+
+ws = websocket.WebSocketApp(
+    'ws://localhost:8080/ws/v2/consumer/persistent/public/default/3/testsub',
+    on_open=on_open,
+    on_message=on_message,
+    on_error=on_error,
+    on_close=on_close
+)
+
+
+ws_thread = Thread(target=ws.run_forever, daemon=True)
+
+def atexit(*args):
+    ws.close()
+    ws_thread.join()
+    exit()
+
+signal.signal(signal.SIGINT, atexit)
+
+ws_thread.start()
+
 while True:
-    msg = json.loads(ws.recv())
-    print(msg['properties'])
-    # Acknowledge successful processing
-    ws.send(json.dumps({'messageId' : msg['messageId']}))
+    sleep(1)
