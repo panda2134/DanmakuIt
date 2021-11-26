99bf7c934ceff38636c0b3540ab324d80b46f2ac
panda2134 <ljypanda@live.com>
2021-11-26 21:10:50 +0800

    feat: better room id generation

--- src/dashboard/app/utils/__init__.py
+++ src/dashboard/app/utils/__init__.py
@@ -6,5 +6,5 @@ passcode_charset -= {'l', '1', 'I', '0', 'O'}
 
 
 def generate_passcode(length: int) -> str:
-    passcode = ''.join([secrets.choice(list(passcode_charset)) for i in range(length)])
+    passcode = ''.join([secrets.choice(list(passcode_charset)) for _ in range(length)])
     return passcode
--- src/dashboard/app/utils/room.py
+++ src/dashboard/app/utils/room.py
@@ -1,20 +1,25 @@
-from datetime import datetime
+import struct
+import time
 from typing import Tuple
-import random
-import string
+import secrets
+from hashlib import sha256
 
 from app.utils import generate_passcode
 from app.db import db
 from app.config import app_config
 
 
+def generate_room_id():
+    h = sha256()
+    h.update(struct.pack('f', time.time()))
+    h.update(secrets.token_bytes(32))
+    return int.from_bytes(h.digest()[-4:], 'little')
+
 
 async def generate_room_credentials() -> Tuple[str, str]:
     while True:
-        number_part1 = int(datetime.utcnow().timestamp())
-        number_part2 = random.randint(1, 1 << 5)
-        room_id = (number_part1 << 5) | number_part2 # todo: better id generation to avoid enumerating
-        if not await db['room'].find_one({ 'room_id': room_id }):
+        room_id = generate_room_id()
+        if not await db['room'].find_one({'room_id': room_id}):
             break
     room_passcode = generate_passcode(app_config.room.room_passcode_length)
-    return str(room_id), room_passcode
+    return str(room_id).ljust(10, '0'), room_passcode
