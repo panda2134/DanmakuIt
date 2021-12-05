7832b8f86d09fe3d24b317174ec161a2b2fc48c7
YouJiacheng <1503679330@qq.com>
2021-12-05 14:36:35 +0800

    fix

--- test/ws_client.py
+++ test/ws_client.py
@@ -42,7 +42,7 @@ class HTTPBearerAuth(requests.auth.AuthBase):
 
 
 schema = 'ws'
-domain = 'http://82.156.115.62:8000' # 'localhost:8000' # 'se-srv2.panda2134.site'
+domain = '82.156.115.62:8000' # 'localhost:8000' # 'se-srv2.panda2134.site'
 
 
 if __name__ == '__main__':
