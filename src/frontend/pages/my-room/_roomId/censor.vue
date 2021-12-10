5d7aa55c9ad47d29e1b80f8b69d482ad4a478859
panda2134 <ljypanda@live.com>
2021-12-11 01:36:18 +0800

    feat(frontend): blacklist keyword management

--- src/frontend/pages/my-room/_roomId/censor.vue
+++ src/frontend/pages/my-room/_roomId/censor.vue
@@ -1,19 +1,14 @@
 <template>
   <v-container>
     <room-detail-header :room-id="room.room_id" :room-name="room.name" />
-    <device-info-card class="mb-5" :room-id="room.room_id" :passcode="room.room_passcode" />
-    <connected-device-list-card class="mb-5" :room-id="room.room_id" />
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Room } from '~/openapi/models'
-import DeviceInfoCard from '~/components/DeviceInfoCard.vue'
-import ConnectedDeviceListCard from '~/components/ConnectedDeviceListCard.vue'
 
 export default Vue.extend({
-  components: { DeviceInfoCard, ConnectedDeviceListCard },
   async asyncData ({ $api, params }) {
     const room = await $api['/room/{room_id}'].get(params.roomId)
     return { room }
