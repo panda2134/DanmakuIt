30e8bff065ad06fe8b63a2569a4946d1ea8f64a2
panda2134 <ljypanda@live.com>
2021-12-11 04:26:25 +0800

    fix(frontend): logout

--- src/frontend/pages/logout.vue
+++ src/frontend/pages/logout.vue
@@ -3,6 +3,7 @@ import Vue from 'vue'
 
 export default Vue.extend({
   beforeMount () {
+    localStorage.removeItem('token')
     this.$axios.setToken(false)
     this.$router.push('/')
     this.$toast.success('退出登录成功', { y: 'top' })
