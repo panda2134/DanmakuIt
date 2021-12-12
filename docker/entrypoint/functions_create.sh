9e638aa01c8efeca3ce85129eb7a083cd148675b
YouJiacheng <1503679330@qq.com>
2021-12-12 21:59:29 +0800

    fix: tagger reload

--- docker/entrypoint/functions_create.sh
+++ docker/entrypoint/functions_create.sh
@@ -4,6 +4,11 @@ echo "-- Wait for Pulsar service to be ready"
 until curl http://localhost:8080/metrics > /dev/null 2>&1 ; do sleep 1; done
 echo "-- Pulsar service is ready -- Create functions"
 sleep 10
+bin/pulsar-admin functions delete \
+  --tenant public \
+  --namespace default \
+  --name tagger
+
 bin/pulsar-admin functions create \
   --py ./tagger.py \
   --classname tagger.TaggingFunction \
