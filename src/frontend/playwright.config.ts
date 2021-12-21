961094aff11e88d5abb861c1edae099a1932b4a1
panda2134 <ljypanda@live.com>
2021-12-22 00:19:34 +0800

    tests(frontend): made webserver timeout longer

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -4,7 +4,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'yarn build && yarn generate && yarn start',
     port: 3000,
-    timeout: 120 * 1000,
+    timeout: 600 * 1000,
     reuseExistingServer: !process.env.CI
   },
   reporter: process.env.CI ? 'github' : 'list',
