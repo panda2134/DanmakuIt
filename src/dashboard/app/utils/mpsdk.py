34e915e6e8414023989171e1827d4d5d2d8642c8
panda2134 <ljypanda@live.com>
2022-05-04 22:43:09 +0800

    fix(dashboard): use grant_type=client_credential

--- src/dashboard/app/utils/mpsdk.py
+++ src/dashboard/app/utils/mpsdk.py
@@ -3,4 +3,4 @@ import os
 from weixin import WXAPPAPI
 
 appid, app_secret = os.getenv('WECHAT_MP_APPID'), os.getenv('WECHAT_MP_APPSECRET')
-mpsdk = WXAPPAPI(appid=appid, app_secret=app_secret)
+mpsdk = WXAPPAPI(appid=appid, app_secret=app_secret, grant_type='client_credential')
